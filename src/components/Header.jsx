2c04c6872b42a9e268781208d14286e340aa5957
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-17 13:56:53 +0530

    added navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <h1>TechyStar.</h1>
+      <main>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/contact"}>Contact</Link>
+        {/* #about goes to the about part in the same page */}
+        <Link to={"/#about"}>About</Link>
+        <Link to={"/#brands"}>Brands</Link>
+        <Link to={"/services"}>Services</Link>
+      </main>
+    </nav>
+  );
 };
 
 export default Header;
