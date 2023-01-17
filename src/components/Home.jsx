48b745bef8fc0e899a88c69c87495c02068ad835
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-17 20:07:38 +0530

    added third home part and its style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,6 +21,28 @@ const Home = () => {
           </p>
         </div>
       </div>
+
+      <div className="home3">
+        <div>
+          <h1>Who are we?</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+            nihil illum, ratione saepe doloribus in eius distinctio? Nulla quos
+            sint, fuga ea, cupiditate nemo nobis eligendi id laudantium sapiente
+            facere! Quas, cumque ab nam voluptatibus repellendus molestiae
+            deleniti excepturi animi, tempora ipsa minima nemo tempore odit
+            sapiente aspernatur optio, rem quis doloribus est quae. Labore
+            provident suscipit cumque nesciunt excepturi? Consequatur, laborum
+            nemo enim ea cum non doloremque quia est amet facere suscipit quos
+            rerum quam laudantium ipsa ipsam rem officiis quae dolorem
+            asperiores, voluptatum fugiat, debitis perspiciatis! Non, fuga. Ut,
+            assumenda maiores, obcaecati nesciunt saepe iure iste mollitia amet,
+            doloribus vitae corrupti. Laboriosam soluta ut aut odit sed,
+            repellat eligendi ea voluptates in quae pariatur fugit iusto eum
+            quam.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
