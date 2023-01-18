30c09a2fc91bc4d98ad2af1231b6db3c926840ec
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-18 12:27:43 +0530

    added footer and its style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/" target={"blank"}>
+            YouTube
+          </a>
+          <a href="https://www.instagram.com/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://www.github.com/" target={"blank"}>
+            Github
+          </a>
+          <a href="https://www.linkedin.com/" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="https://www.twitter.com/" target={"blank"}>
+            Twitter
+          </a>
+          <a href="https://www.facebook.com/" target={"blank"}>
+            FaceBook
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
